046598902f9b4c709758afa4a7e33e69c2c7fd87
topektopiq <hidayatullohtaufiq0@gmail.com>
2023-12-07 14:20:05 +0700

    login and register page fixed style css

--- modules/auth/auth_view/login.php
+++ modules/auth/auth_view/login.php
@@ -12,7 +12,8 @@ if (session_status() === PHP_SESSION_NONE)
     <title>Ruang Baca</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/login.css">
+    <!-- <link rel="stylesheet" href="assets/css/login.css"> -->
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&display=swap" rel="stylesheet">
@@ -20,41 +21,40 @@ if (session_status() === PHP_SESSION_NONE)
 
 <body>
     <section class="login d-flex">
-        <div class="login-left w-50 h-150">
-            <div class="row justify-content-center" style="margin-top: 150px;">
+        <div class="login-form-container d-flex">
+            <div class="login-register-area d-flex flex-column">
+                <img src="assets/logo.svg" class="logo">
 
-                <div class="col-6">
-                    <div class="header">
-                        <form action="index.php?page=login" method="post">
-                            <img src="assets/logo.svg" class="img-fluid">
-                            <h1>Selamat Datang Kembali!👋🏼</h1>
-                            <p>Masuk dan Pinjam buku yang kamu butuhkan</p>
-                            <div class="login-form nds-input-layout-control">
-                                <label for="floatingInput">Nama Pengguna</label>
-                                <input type="text" class="form-control btn-light primary py-3 mb-4" name="username" placeholder="Masukkan Nama Pengguna" required>
-                            </div>
-                            <div class="login-form nds-input-layout-control mb-4">
-                                <label for="floatingInput">Kata Sandi</label>
-                                <input type="password" class="eye form-control btn-light primary py-3" name="password" placeholder="Masukkan Kata Sandi" required>
-                            </div>
-                            <div class="login-form">
-                                <button class="btn btn-primary w-100" type="submit">Masuk</button>
-                            </div>
-                        </form>
-
-                        <div class="text-center">
-                            <span class="d-inline">Belum punya akun? <a href="/index.php?page=register" class="signup d-inline text-decoration-none">Daftar</a></span>
-                        </div>
+                <div class="login-register-form-and-title d-flex flex-column">
+                    <div>
+                        <h1 class="login-heading">Selamat Datang Kembali!👋🏼</h1>
+                        <p class="login-subtitle">Masuk dan Pinjam buku yang kamu butuhkan</p>
                     </div>
+                    <form action="index.php?page=login" method="post" class="d-flex flex-column" style="gap: 12px;">
+                        <div class="addbook-input-field input-fields d-flex flex-column">
+                            <label for="username">Nama Pengguna</label>
+                            <input type="text" id="username" name="username" placeholder="Masukkan Nama Pengguna" required>
+                        </div>
+                        <div class="addbook-input-field input-fields d-flex flex-column">
+                            <label for="password">Kata Sandi</label>
+                            <input type="password" id="password" name="password" placeholder="Masukkan Kata Sandi" required>
+                        </div>
+                        <div class="login-submit d-flex flex-column">
+                            <button class="enabled full-width" type="submit">Masuk</button>
+                            <p class="d-inline">Belum punya akun? <a href="/index.php?page=register" class="signup d-inline text-decoration-none">Daftar</a></p>
+                        </div>
+                    </form>
                 </div>
+                
             </div>
         </div>
 
-        <div class="login-right w-50 h-100">
-            <img src="assets/login.svg" class="img-fluid" style="margin-top: 150px;">
+        <div class="login-illustration-container d-flex">
+            <img src="assets/login.svg" class="img-fluid">
         </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 
