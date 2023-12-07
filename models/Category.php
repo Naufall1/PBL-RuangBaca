608b32d6f2c4a01e149ef1b79f64aecaf452ed58
Amanda <120579752+mandesss@users.noreply.github.com>
2023-12-07 10:02:00 +0700

    Update Category.php

--- models/Category.php
+++ models/Category.php
@@ -56,8 +56,8 @@ class Category implements IManage
         ";
 
                 $parameters = [
-                        $this->category_name,
                         $this->category_id,
+                        $this->category_name,
                 ];
 
                 $statement = Database::prepare($query);
@@ -70,7 +70,16 @@ class Category implements IManage
         }
     public function delete()
     {
-    }
+                $query = "DELETE FROM category WHERE category_id = ?";
+                $parameters = [
+                        $this->category_id
+                ];
+                $statement = Database::prepare($query);
+                $type = 's';
+                $statement->bind_param($type, ...$parameters);
+
+                $statement->execute();
+        }
 
     public function getId()
     {
