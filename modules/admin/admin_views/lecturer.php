e2531f3c3b7dd02c89c8f8bdfb363001ff8d30a4
topektopiq <hidayatullohtaufiq0@gmail.com>
2023-12-09 20:58:09 +0700

    create add, edit, delete modal for lecturer

--- modules/admin/admin_views/lecturer.php
+++ modules/admin/admin_views/lecturer.php
@@ -84,3 +84,165 @@
     </a>
 </div>
 <!-- End: Pagination View -->
+
+
+<!-- Start: Modal Add Author -->
+<!-- DELETE DISPLAY STYLE FIRST BELOW -->
+<div class="modal" id="modalBuku">
+    <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+        <div class="modal-content modal-custom-single-col">
+            <div class="modal-header border-0 d-flex">
+                <div class="d-flex" style="padding: 0; width: 100%;">
+
+                    <div class="d-flex flex-column align-content-between " style="width: 100%;">
+                        <h3 class="modal-heading" id="">Tambah Data</h3>
+                    </div>
+
+                    <button type="button" data-dismiss="modal" aria-label="Close" class="close-button"
+                        onclick="closeModal(this);" id="book">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+                            <path stroke="#1B1B1B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="M5 15 15 5m0 10-5-5-5-5" />
+                        </svg>
+                    </button>
+
+                </div>
+            </div>
+
+            <div class="modal-body">
+
+                <form class=" flex-column d-flex">
+                    <div class="modal-form-addbook-area d-flex flex-column">
+                        <div class="addbook-input-field input-fields d-flex flex-column">
+                            <label for="NIDN">NIDN</label>
+                            <input type="text" id="NIDN" name="NIDN" placeholder="Masukkan NIDN">
+                        </div>
+                        <div class="addbook-input-field input-fields d-flex flex-column">
+                            <label for="lecturer_name">Nama Dosen</label>
+                            <input required type="text" id="lecturer_name" name="lecturer_name" placeholder="Masukkan Nama Dosen">
+                        </div>                            
+                    </div>
+
+                    <div class="d-flex modal-button-group" style="gap: 12px;">
+                        <button type="submit" class="enabled modal-button-top-margin" id="tambah" name="author" onclick="">Tambah</button>
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- End: Modal Add Author -->
+
+
+<!-- Start: Modal Edit Author -->
+<!-- DELETE DISPLAY STYLE FIRST BELOW -->
+<div class="modal" id="modalBuku">
+    <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+        <div class="modal-content modal-custom-single-col">
+            <div class="modal-header border-0 d-flex">
+                <div class="d-flex" style="padding: 0; width: 100%;">
+
+                    <div class="d-flex flex-column align-content-between " style="width: 100%;">
+                        <h3 class="modal-heading" id="">Edit Data</h3>
+                    </div>
+
+                    <button type="button" data-dismiss="modal" aria-label="Close" class="close-button"
+                        onclick="closeModal(this);" id="book">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+                            <path stroke="#1B1B1B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="M5 15 15 5m0 10-5-5-5-5" />
+                        </svg>
+                    </button>
+
+                </div>
+            </div>
+
+            <div class="modal-body">
+
+                <form class=" flex-column d-flex">
+
+                    <div class="modal-form-addbook-areas d-flex">
+
+                        <div class="modal-form-addbook-area d-flex flex-column">
+                            <div class="addbook-input-field input-fields d-flex flex-column">
+                                <label for="NIDN">NIDN</label>
+                                <input value="[NIDN]" disabled type="text" id="NIDN" name="NIDN"
+                                    placeholder="Masukkan NIDN">
+                            </div>
+                            <div class="addbook-input-field input-fields d-flex flex-column">
+                                <label for="lecturer_name">Nama Dosen</label>
+                                <input value="[FILLED LECTURER NAME]"  required type="text" id="lecturer_name" name="lecturer_name" placeholder="Masukkan Nama Dosen">
+                            </div>  
+                        </div>
+
+                    </div>
+
+                    <div class="d-flex modal-button-group" style="gap: 12px;">
+
+                        <button type="submit" class="enabled modal-button-top-margin" id="tambah" name="author"
+                            onclick="">Simpan</button>
+
+                    </div>
+
+                </form>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+<!-- End: Modal Edit Author -->
+
+
+<!-- Start: Modal Delete-->
+<!-- DELETE DISPLAY STYLE FIRST BELOW -->
+<div class="modal" id="modalBuku">
+    <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+        <div class="modal-content modal-custom-delete">
+            <div class="modal-header border-0 d-flex">
+                <div class="d-flex" style="padding: 0; width: 100%;">
+
+                    <div class="d-flex flex-column align-content-between " style="width: 100%;">
+                        <h3 class="modal-heading" id="">Hapus Data</h3>
+                    </div>
+
+                    <!-- <button type="button" data-dismiss="modal" aria-label="Close" class="close-button"
+                        onclick="closeModal(this);" id="book">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+                            <path stroke="#1B1B1B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="M5 15 15 5m0 10-5-5-5-5" />
+                        </svg>
+                    </button> -->
+                    
+                </div>
+            </div>
+            
+            <div class="modal-body">
+                
+                <!-- <form class=" flex-column d-flex"> -->
+                    <p class="delete-confirmation">
+                        Apakah Anda yakin ingin menghapus Dosen dengan NIDN <span>BK0001</span>?
+                    </p>
+                    
+                    
+                    <div class="d-flex modal-button-group" style="gap: 12px;">
+                        
+                        <button type="button" class="enabled danger modal-button-top-margin" id="hapus" name="author" onclick="">Hapus</button>
+
+                        <!-- BUTTON BATAL GA GELEM CLOSE, BLM KETEMU SOLUSINYA -->
+                        <button type="button" data-dismiss="modal" aria-label="Close" class="enabled secondary modal-button-top-margin" onclick="closeModal(this);" id="modalBuku">Batal</button>
+                        <!-- BUTTON BATAL GA GELEM CLOSE, BLM KETEMU SOLUSINYA -->
+                    </div>
+
+                <!-- </form> -->
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+<!-- End: Modal Delete-->
