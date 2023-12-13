287282c2f5e7f600c8b04acb9acff7699a51683f
topektopiq <hidayatullohtaufiq0@gmail.com>
2023-12-13 12:16:53 +0700

    fixed style user's summarize

--- modules/member/member_views/history.php
+++ modules/member/member_views/history.php
@@ -4,11 +4,11 @@
 
     <!-- Start: Total Buku -->
     <div class="summarize-item d-flex flex-column">
-        <div class="icon-container d-flex justify-content-center align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" margin="0">
-                <path stroke="#1B84FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2.916 15V5.833c0-3.333.833-4.166 4.167-4.166h5.833c3.333 0 4.167.833 4.167 4.166v8.334c0 .116 0 .233-.009.35" />
-                <path stroke="#1B84FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5.291 12.5h11.792v2.917a2.92 2.92 0 0 1-2.917 2.916H5.833a2.92 2.92 0 0 1-2.917-2.916v-.542A2.38 2.38 0 0 1 5.291 12.5Zm1.375-6.667h6.667M6.666 8.75h4.167" />
-            </svg>
+        <div class="icon-container icon-container-danger d-flex justify-content-center align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+            <path stroke="#E20000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10 18.333c4.583 0 8.333-3.75 8.333-8.333S14.583 1.667 10 1.667 1.667 5.417 1.667 10s3.75 8.333 8.333 8.333Zm0-11.666v4.166m-.004 2.5h.007"/>
+        </svg>
+
         </div>
         <div class="summmarize-content">
             <p class="summarize-title">Tanggungan Kompen</p>
@@ -20,12 +20,12 @@
     </div>
     <!-- End: Total Buku -->
 
-    <!-- Start: Total Skripsi -->
+    <!-- Start: Dikonfirmasi -->
     <div class="summarize-item d-flex flex-column">
-        <div class="icon-container d-flex justify-content-center align-items-center">
+        <div class="icon-container icon-container-success d-flex justify-content-center align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" margin="0">
-                <path stroke="#1B84FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M18.532 3.892V13.95c0 .8-.65 1.55-1.45 1.65l-.275.033c-1.817.242-4.617 1.167-6.217 2.05-.216.125-.575.125-.8 0l-.033-.016c-1.6-.875-4.392-1.792-6.2-2.034l-.242-.033c-.8-.1-1.45-.85-1.45-1.65V3.883a1.64 1.64 0 0 1 1.8-1.658c1.75.142 4.4 1.025 5.884 1.95l.208.125c.242.15.642.15.883 0l.142-.092a12.85 12.85 0 0 1 1.917-.941v3.4l1.666-1.109 1.667 1.109v-4.35c.225-.042.442-.067.642-.084h.05a1.642 1.642 0 0 1 1.808 1.659Zm-8.332.683v12.5" />
-                <path stroke="#1B84FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16.033 2.317v4.35l-1.667-1.109-1.667 1.109v-3.4c1.092-.434 2.309-.784 3.334-.95Z" />
+                <path stroke="#14AE5C" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M18.532 3.892V13.95c0 .8-.65 1.55-1.45 1.65l-.275.033c-1.817.242-4.617 1.167-6.217 2.05-.216.125-.575.125-.8 0l-.033-.016c-1.6-.875-4.392-1.792-6.2-2.034l-.242-.033c-.8-.1-1.45-.85-1.45-1.65V3.883a1.64 1.64 0 0 1 1.8-1.658c1.75.142 4.4 1.025 5.884 1.95l.208.125c.242.15.642.15.883 0l.142-.092a12.85 12.85 0 0 1 1.917-.941v3.4l1.666-1.109 1.667 1.109v-4.35c.225-.042.442-.067.642-.084h.05a1.642 1.642 0 0 1 1.808 1.659Zm-8.332.683v12.5" />
+                <path stroke="#14AE5C" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16.033 2.317v4.35l-1.667-1.109-1.667 1.109v-3.4c1.092-.434 2.309-.784 3.334-.95Z" />
             </svg>
         </div>
         <div class="summmarize-content">
@@ -36,14 +36,14 @@
             </p>
         </div>
     </div>
-    <!-- End: Total Skripsi -->
+    <!-- End: Dikonfirmasi -->
 
-    <!-- Start: Total Anggota -->
+    <!-- Start: Dipinjam -->
     <div class="summarize-item d-flex flex-column">
-        <div class="icon-container d-flex justify-content-center align-items-center">
+        <div class="icon-container icon-container-warning d-flex justify-content-center align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" margin="0">
-                <path stroke="#1B84FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M18.532 3.892V13.95c0 .8-.65 1.55-1.45 1.65l-.275.033c-1.817.242-4.617 1.167-6.217 2.05-.216.125-.575.125-.8 0l-.033-.016c-1.6-.875-4.392-1.792-6.2-2.034l-.242-.033c-.8-.1-1.45-.85-1.45-1.65V3.883a1.64 1.64 0 0 1 1.8-1.658c1.75.142 4.4 1.025 5.884 1.95l.208.125c.242.15.642.15.883 0l.142-.092a12.85 12.85 0 0 1 1.917-.941v3.4l1.666-1.109 1.667 1.109v-4.35c.225-.042.442-.067.642-.084h.05a1.642 1.642 0 0 1 1.808 1.659Zm-8.332.683v12.5" />
-                <path stroke="#1B84FF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16.033 2.317v4.35l-1.667-1.109-1.667 1.109v-3.4c1.092-.434 2.309-.784 3.334-.95Z" />
+                <path stroke="#F1CB01" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M18.532 3.892V13.95c0 .8-.65 1.55-1.45 1.65l-.275.033c-1.817.242-4.617 1.167-6.217 2.05-.216.125-.575.125-.8 0l-.033-.016c-1.6-.875-4.392-1.792-6.2-2.034l-.242-.033c-.8-.1-1.45-.85-1.45-1.65V3.883a1.64 1.64 0 0 1 1.8-1.658c1.75.142 4.4 1.025 5.884 1.95l.208.125c.242.15.642.15.883 0l.142-.092a12.85 12.85 0 0 1 1.917-.941v3.4l1.666-1.109 1.667 1.109v-4.35c.225-.042.442-.067.642-.084h.05a1.642 1.642 0 0 1 1.808 1.659Zm-8.332.683v12.5" />
+                <path stroke="#F1CB01" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16.033 2.317v4.35l-1.667-1.109-1.667 1.109v-3.4c1.092-.434 2.309-.784 3.334-.95Z" />
             </svg>
         </div>
         <div class="summmarize-content">
@@ -54,9 +54,9 @@
             </p>
         </div>
     </div>
-    <!-- End: Total Anggota -->
+    <!-- End: Dipinjam -->
 
-    <!-- Start: Total Peminjaman -->
+    <!-- Start: Selesai -->
     <div class="summarize-item d-flex flex-column">
         <div class="icon-container d-flex justify-content-center align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" margin="0">
@@ -72,7 +72,7 @@
             </p>
         </div>
     </div>
-    <!-- End: Total Peminjaman -->
+    <!-- End: Selesai -->
 
 
 </div>
