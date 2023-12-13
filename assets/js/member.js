6bc10b9726a5cfc8e0c70f9109eeb909e7d5b80f
topektopiq <hidayatullohtaufiq0@gmail.com>
2023-12-13 19:06:58 +0700

    fixed filter catalog

--- assets/js/member.js
+++ assets/js/member.js
@@ -114,8 +114,6 @@ function loadModule(moduleName) {
                 // console.log($(this).attr('id'));
             });
 
-            loadFilter();
-
             // History
             $('.tab-menu .tab-item').click(function () {
                 $('.tab-item-active').addClass('tab-item');
